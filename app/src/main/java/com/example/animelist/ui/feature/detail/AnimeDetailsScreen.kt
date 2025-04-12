c1a4df82ec88c0d5b973848a8444855054baf577
haphongle <hphongl93@gmail.co>
2025-04-12 22:19:55 +1000

    added ExpandableTextView to details screen

--- app/src/main/java/com/example/animelist/ui/feature/detail/AnimeDetailsScreen.kt
+++ app/src/main/java/com/example/animelist/ui/feature/detail/AnimeDetailsScreen.kt
@@ -1,27 +1,42 @@
 package com.example.animelist.ui.feature.detail
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.animelist.R
 import com.example.animelist.domain.mockModel.mock
 import com.example.animelist.domain.model.AnimeDetails
 import com.example.animelist.ui.feature.dashboard.component.FullScreenLoading
@@ -81,16 +96,61 @@ private fun AnimeDetailsSuccessScreen(
             BasicInfo(animeDetails = animeDetails)
 
             animeDetails.description?.let {
-                Text(
+                ExpandableTextView(
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(top = AppTheme.dimension.spaceM),
-                    textAlign = TextAlign.Center,
                     text = it,
-                    color = MaterialTheme.colorScheme.onPrimary
+                    textLengthToCutOff = 400
                 )
             }
             StreamingEpisodes(streamingEpisode = animeDetails.streamingEpisodes)
+        }
+    }
+}
+
+@Composable
+private fun ExpandableTextView(
+    modifier: Modifier = Modifier,
+    text: String,
+    textLengthToCutOff: Int
+) {
+    var isExpanded by remember {
+        mutableStateOf(false)
+    }
+    if (text.length < textLengthToCutOff) {
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = AppTheme.dimension.spaceM),
+            textAlign = TextAlign.Center,
+            text = text,
+            color = MaterialTheme.colorScheme.onPrimary
+        )
+    } else {
+        Column(
+            modifier = modifier
+                .animateContentSize(animationSpec = tween(500)),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                text = if (isExpanded) text else text.take(textLengthToCutOff),
+                color = MaterialTheme.colorScheme.onPrimary
+            )
+            Spacer(modifier = Modifier.height(AppTheme.dimension.spaceL))
+            Text(
+                text = if (isExpanded) stringResource(R.string.show_less) else stringResource(R.string.show_more),
+                modifier = Modifier
+                    .width(300.dp)
+                    .border(width = 2.dp, color = MaterialTheme.colorScheme.onPrimary, shape = RoundedCornerShape(AppTheme.dimension.spaceL))
+                    .clickable { isExpanded = !isExpanded }
+                    .padding(AppTheme.dimension.spaceM),
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.onPrimary,
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
